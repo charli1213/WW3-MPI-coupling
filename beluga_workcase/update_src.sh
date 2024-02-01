e30cbefa2453384c8ebf37f9d3cdd5d96bbb718c
Charles-Édouard Lizotte <charli1213@protonmail.com>
2024-02-01 16:29:07 -0500

    makefile de wwatch

--- beluga_workcase/update_src.sh
+++ beluga_workcase/update_src.sh
@@ -11,7 +11,7 @@ rm *.nc
 rm *.inp
 
 # 2. Copying ww3_*.inp files into case
-ww3_inp_dir=$HOME/celiz2/projects/def-lpnadeau/celiz2/mpi_work/ww3_inp_files
+ww3_inp_dir=$HOME/projects/def-lpnadeau/celiz2/mpi_work/ww3_inp_files
 cp $ww3_inp_dir/*.inp .
 echo " > Copying .inp files from ${ww3_inp_dir}"
 rm exec
---
+++ ww3_modified_subroutines/bin/make_oxygen.sh
@@ -0,0 +1,44 @@
+#!/bin/bash                                                                                                          # ---------------------------------------------------------------------------- #
+# This bash script precompile AND compile the Wavewatch                        #
+# III model in MPI mode for Beluga.                                            #
+#                                                                              #
+#                                                C.-É. Lizotte                 #
+#                                                 febuary 2023                 #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+#
+# 1. Set-up - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo " > Setting-up directories."
+wwatch_dir=$HOME/projects/def-lpnadeau/celiz2/wavewatch3/
+switch_file=lizotte
+#
+# 2. Modules Intel  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo " > Loading Intel modules"
+module load intel
+#
+# 3. cleaning - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo " > Cleaning model "
+w3_new all
+w3_clean
+#
+# 4. Précompilation et compilation sans MPI (avec Intel)  - - - - - - - - - - -
+# n.b we do that because netCDF module only works with Ifort on compute
+#     canada (sadly). 
+echo " > Setting up switches and compiler for non MPI part (Intel)."
+echo 'n' | w3_setup $wwatch_dir -s $switch_file -c Intel
+echo " > Compiling non MPI part"
+w3_make
+#
+# 5. Précompilation et compilation avec MPI (avec Gfortran) - - - - - - - - - -
+# n.b. we do that because fishpack only works with gfortran, therefor we can
+#      only use mpi with a gfortran compiled code...
+echo " > Loading GCC module"
+module load gcc
+echo ' > Setting up compiler for MPI (Gfortran)'
+echo 'n' | w3_setup $wwatch_dir -s $switch_file -c gfortran
+echo " > Finally compiling MPI part"
+make_MPI
+#
+# 6. Fin  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+module load gcc
+echo " >>> End"
